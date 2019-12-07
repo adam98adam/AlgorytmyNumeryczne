5d04bd820c26f8e9eb16db9b22e1b7e11d04b99f
adam98adam <adamzebr98@gmail.com>
2019-12-07 04:05:08 +0100

    Zadanie 2 V1

---
+++ Zadanie 2/GaussFloat/Generate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+
+namespace GaussFloat
+{
+    class Generate
+    {
+        public static int N = 10;
+        public float[,] numbersA = new float[N, N];
+        public float[] numbersX = new float[N];
+        int minRandom = (int)(Math.Pow(2, 16) * -1);
+        int maxRandom = (int)(Math.Pow(2, 16) - 1);
+        int divaider = (int)(Math.Pow(2, 16));
+
+        public void generateA()
+        {
+
+            Random rnd = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    numbersA[i, j] = (float)rnd.Next(minRandom, maxRandom) / (float)divaider;
+
+                }
+            }
+        }
+
+        public void generateX()
+        {
+
+            Random rnd = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                numbersX[i] = (float)rnd.Next(minRandom, maxRandom) / (float)divaider;
+
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Zadanie 2/GaussFloat/PrintMatrix.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GaussFloat
+{
+    class PrintMatrix
+    {
+        public static int N = 10;
+        public void printA(float[,] numbersA)
+        {
+            Console.WriteLine("A Matrix : ");
+            Console.WriteLine();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(numbersA[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        public void printXB(float[] numbersXB,Boolean xb)
+        {
+            Console.WriteLine();
+            if(xb == true)
+                Console.WriteLine("X Matrix : ");
+            else
+                Console.WriteLine("B Matrix : ");
+            Console.WriteLine();
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(numbersXB[i]);
+            }
+
+        }
+
+        public void printAB(float[,] numbersAB)
+        {
+            Console.WriteLine();
+            Console.WriteLine("AB Matrix : ");
+            Console.WriteLine();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N + 1; j++)
+                {
+                    if (j == N)
+                    {
+                        Console.WriteLine("|" + numbersAB[i, j] + "\t");
+                    }
+                    else
+                        Console.Write(numbersAB[i, j] + "\t");
+
+                }
+
+
+            }
+        }
+    }
+}
+
+    
